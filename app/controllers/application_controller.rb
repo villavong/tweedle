5a8d6d1decb608c659ac6d44e7727d9f4e583e4c
villavong <asyao@naver.com>
2016-06-24 15:39:35 +0900

    application registrations users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-before_action  :configure_permitted_parameters, if: :devise_controller?
+  before_action  :configure_permitted_parameters, if: :devise_controller?
 
-helper_method :mailbox
-before_filter :autocomplete
+  helper_method :mailbox
+  before_filter :autocomplete
+
+
 
 private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,16 @@ class UsersController <ApplicationController
 	def index
 
 		@search = User.ransack(params[:q])
+		@results = @search.result
+		@arrUsers = @results.to_a
+		@revisers = Reviser.all
+
 		# @user = @search.result.order("created_at DESC").to_a.uniq
 
 		# @country = User.uniq.pluck(:country)
 		# @city = User.uniq.pluck(:city)
 		# @school = User.uniq.pluck(:school)
 		# @major = User.uniq.pluck(:major)
-
-
-		@results = @search.result
-		@arrUsers = @results.to_a
-
-		@revisers = Reviser.all
 
 	end
 
