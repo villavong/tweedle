468f00dbcd76614e9e8fe21221603c10f5a301df
villavong <asyao@naver.com>
2016-06-24 15:50:52 +0900

    sendgrid profile only in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'asyao@naver.com',
-  :password => 'genius89',
-  :domain => 'https://gentle-sierra-92965.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
