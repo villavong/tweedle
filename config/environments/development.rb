21e901c57f70f084991a5dfdc9ad7d14ee33af9e
villavong <asyao@naver.com>
2016-06-24 15:12:15 +0900

    sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
    config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+
+    config.action_mailer.smtp_settings = {
+
       :address => "email-smtp.us-west-2.amazonaws.com",
       :port => 587, # Port 25 is throttled on AWS
       :user_name => "AKIAJ3PP26PYSDJHPURQ", # Your SMTP user here.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,28 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
     # config/environments/production.rb
-config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
+# config.paperclip_defaults = {
+#   storage: :s3,
+#   s3_credentials: {
+#     bucket: ENV.fetch('S3_BUCKET_NAME'),
+#     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+#     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+#     s3_region: ENV.fetch('AWS_REGION'),
+#   }
+# }
+config.action_mailer.default_url_options = { host: 'https://gentle-sierra-92965.herokuapp.com/' }
+config.action_mailer.delivery_method = :smtp
+
+
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
+
 end
