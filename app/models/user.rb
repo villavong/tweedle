6f6bf59d636a0dba96a0ba1842c010a1efe733dc
villavong <asyao@naver.com>
2016-06-24 15:15:25 +0900

    confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :omniauthable, :confirmable
 
 has_many :posts, dependent: :destroy
 has_many :photos
