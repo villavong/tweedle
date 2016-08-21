c4780a98e412242510c78030bc6e76527c7e3b66
villavong <asyao@naver.com>
2016-08-21 20:26:07 +0900

    mailboxer

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -4,7 +4,7 @@ Mailboxer.setup do |config|
   config.uses_emails = false
 
   #Configures the default from for emails sent for Messages and Notifications
-  config.default_from = "hello@senpaicounsel.com"
+  config.default_from = "hello@tweedlemate.com"
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email
