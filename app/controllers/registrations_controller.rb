e7925d14426288fc1c3baa14d427d9716b98ddb1
villavong <asyao@naver.com>
2016-08-31 03:30:35 +0900

    Korean version

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,7 +15,13 @@ class RegistrationsController < Devise::RegistrationsController
 		def after_sign_up_path_for(resource)
 	    edit_user_registration_path
 	  end
-		def after_sign_in_path_for(resource)
-	    request.env['omniauth.origin'] || stored_location_for(resource) || edit_user_registration_path
-	  end
+		def after_sign_in_path_for(resource_or_scope)
+		    if resource.sign_in_count == 1
+					edit_user_registration_path
+
+		    else
+		       root_path
+		    end
+		end
+
 end
