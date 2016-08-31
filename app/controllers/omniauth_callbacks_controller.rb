874f1d7a16003aa836e6708553ae9df9675122b5
villavong <asyao@naver.com>
2016-09-01 02:55:53 +0900

    Korean version

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,11 +5,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		if @user.persisted?
 			sign_in_and_redirect @user, :event => :authentication
 
-			respond_to do |format|
-				format.html { redirect_to edit_user_registration, notice: '프로필을 수정해 주세요 :)' }
-				format.json { head :no_content }
-			end
-			# set_flash_message(:notice, :success, :kind => "Facebook. 프로필을 수정하세요!") if is_navigational_format?
+
+			set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
 
 
 		else
