5d468ddb3da0a56993c1989a2d9f42f2784cc0e8
villavong <asyao@naver.com>
2016-08-31 14:11:23 +0900

    Korean version

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,10 +12,12 @@ class RegistrationsController < Devise::RegistrationsController
     def after_update_path_for(resource)
       user_path(resource)
     end
-		
+
 		def after_sign_up_path_for(resource)
 		    if resource.sign_in_count == 1
 					edit_user_registration_path
+					flash[:success] = '프로필을 수정해주세요!'
+
 		    else
 		       root_path
 		    end
